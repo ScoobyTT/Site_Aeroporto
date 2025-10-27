310efb10e1a53f9f8b700d33802840ab9a70f671
Christian Amós <christianamosa@gmail.com>
2025-10-27 11:29:46 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,16 +1,9 @@
 from app import db
 from datetime import datetime
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data_envio = db.Column(db.DateTime, default=datetime.utcnow)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.username}>'
 
-class Cadastro(db.Model):
+class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -18,4 +11,4 @@ class Cadastro(db.Model):
     data_envio = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'<Cadastro {self.nome}>'
+        return f'<Usuario {self.nome}>'
